40cc56abf794790bbe546d29c04f7ae3edd8ef0f
Julia Vu <juliadvu@gmail.com>
2020-05-15 10:52:06 -0500

    server file updated

--- server.js
+++ server.js
@@ -1,31 +1,20 @@
-// const express = require('express');
 
-// const projectsRouter = require('./projects/projects-router');
+const express = require('express')
 
-// const db = require('./data/db-config.js');
-
-// const server = express();
-
-// server.use(express.json());
-
-// server.use('/api/projects', projectsRouter)
-// // server.use('/api/resources', resourcesRouter)
-// // server.use('/api/tasks', tasksRouter)
-
-// module.exports = server;
-
-
-const express = require('express');
-
-const RecipeRouter = require('./projects/projects-router')
+const resourcesRouter = require('./resources/resources-router');
+const projectsRouter = require('./projects/projects-router');
+const tasksRouter = require('./tasks/tasks-router');
 
 const db = require('./data/db-config.js');
 
 const server = express();
-
 server.use(express.json());
 
-server.use('/api/recipes', RecipeRouter)
+server.use('/api/projects', projectsRouter)
+server.use('/api/resources', resourcesRouter)
+server.use('/api/tasks', tasksRouter)
 
 module.exports = server;
+
+
 
